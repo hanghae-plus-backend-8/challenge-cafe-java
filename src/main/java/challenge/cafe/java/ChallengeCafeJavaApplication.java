976e340f4cb8efe290237f217c34b92cffe5fd84
discphy <dinophy@nate.com>
2025-02-14 23:59:57 +0900

    :sparkles: 프로젝트 초기 세팅

---
+++ src/main/java/challenge/cafe/java/ChallengeCafeJavaApplication.java
@@ -0,0 +1,13 @@
+package challenge.cafe.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCafeJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeCafeJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/challenge/cafe/java/ChallengeCafeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package challenge.cafe.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCafeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
